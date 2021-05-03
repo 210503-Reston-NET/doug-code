c1b71d41c6f0fec574bfbd5af3496fe2e6a63087
daustindev <daustindev@gmail.com>
2021-05-03 16:36:37 -0500

    added first program

---
+++ Day 1 Program/GreetingsMortals/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreetingsMortals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string customGreeting;
+            customGreeting = "Greetings Mortals";
+            Console.WriteLine(customGreeting);
+        }
+    }
+}
